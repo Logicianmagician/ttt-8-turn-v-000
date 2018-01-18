63ba9776537340ed193481787fc77776a77163c6
Andrew Kairys <andrewkairys@gmail.com>
2018-01-18 19:50:32 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,7 @@ def position_taken?(board, index)
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = "X" || "O"
+  index = input
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   if input_to_index(input) == true || position_taken?(board, index) == true || valid_move?(board, index) == true
      move(array, index, value = "X")
