6e0fd607a1ac842b9afd42b5590c473d18dfb5dc
Andrew Kairys <andrewkairys@gmail.com>
2018-01-18 20:07:03 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,7 +41,7 @@ def turn(board)
   index = input_to_index(input)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   if input_to_index(input) == true || position_taken?(board, index) == true || valid_move?(board, index) == true
-     move(array, index, value = "X")
+     move(board)
  else
     turn(board)
  end
